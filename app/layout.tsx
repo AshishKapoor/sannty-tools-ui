2632b2cd4a7ef633a74b0b226e0bb7ba82571699
Ashish Kapoor <swiftobjc@gmail.com>
2025-04-10 05:43:41 +0000

    minor update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mission Control",
+  title: "Mission Control | Sannty Tools",
   description: "tools.sannty.in",
 };
 
